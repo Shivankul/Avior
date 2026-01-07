9fee97d5e3c31435e3f1054452825d78b96e7727
Shivankul trivedi <139231547+Shivankul@users.noreply.github.com>
2026-01-07 14:12:37 +0530

    added login, register and profile page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<UserLayout />}>
         <Route index element={<Home />} />
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/profile" element={<Profile/>}/>
       </Route>
 
       {/* Admin Layout */}
---
+++ Client/src/pages/Login.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import LoginGurl from "../assets/logingurl.jpg";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      alert("All fields are required");
+      return;
+    }
+    const userData = {
+      email,
+      password,
+    };
+    setEmail("");
+    setPassword("");
+    console.log("User Login: ",email, password);
+  };
+  return (
+    <div className="flex">
+      <div className="w-full md:w-1/2 flex flex-col justify-center items-center p-8 md:p-12">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-md p-8 rounded-lg border shadow-sm"
+        >
+          <div className="flex justify-center mb-6">
+            <h2 className="font-semibold text-xl">Avior</h2>
+          </div>
+          <h2 className="font-bold text-2xl text-center mb-6">Hey there !👋🏻</h2>
+          <p className="text-center mb-6 tracking-tighter">
+            LOGIN BUDDY TO PURCHASE 🛍️
+          </p>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-sm font-semibold mb-2">
+              Email :
+            </label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full p-2 border rounded border-gray-400"
+              placeholder="Enter Your Email Address..."
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">
+              Password :
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full p-2 border rounded border-gray-400 mb-2"
+              placeholder="Enter Your Password..."
+            />
+          </div>
+          <button className="w-full bg-black text-white p-2 rounded-lg font-semibold hover:bg-gray-800 transition">
+            Sign In
+          </button>
+          <p className="text-sm tracking-tighter text-center mt-4">
+            Don't have an Account ?
+            <Link to="/register" className="text-blue-500">
+              {" "}
+              Register
+            </Link>
+          </p>
+        </form>
+      </div>
+      <div className="hidden md:block w-1/2 bg-gray-800">
+        <div className="h-full flex flex-col justify-center items-center">
+          <img
+            src={LoginGurl}
+            alt="Login to Account"
+            className="object-cover h-[700px] w-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
